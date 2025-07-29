f9a06c24f59780e73cd152623dbacba93106c1d2
ATAGWE ROGER <atagweroger2@gmail.com>
2025-07-29 21:13:28 +0100

    Js changes

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,27 +1,15 @@
- document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
   const menuBtn = document.getElementById('menuBtn');
   const sidebar = document.getElementById('sidebar');
   const backdrop = document.getElementById('backdrop');
-  const disabledBtn = document.querySelector(".disabled")
-  disabledBtn.onclick(() => {
-  alert("HAHA!! DON'T DELETE FROM MY DATABASE BUDDY😎")
-})
+
   const toggleMenu = () => {
     sidebar.classList.toggle('active');
-    if (sidebar.classList.contains('active')) {
-      backdrop.classList.add('show');
-    } else {
-      backdrop.classList.remove('show');
-      setTimeout(() => (backdrop.style.display = 'none'), 300);
-    }
+    backdrop.classList.toggle('show');
   };
 
   if (menuBtn && sidebar && backdrop) {
-    menuBtn.addEventListener('click', () => {
-      backdrop.style.display = 'block';
-      setTimeout(toggleMenu, 10); 
-    });
+    menuBtn.addEventListener('click', toggleMenu);
     backdrop.addEventListener('click', toggleMenu);
   }
 });
-
